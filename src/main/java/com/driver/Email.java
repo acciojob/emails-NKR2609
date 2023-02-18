784bf7e5ef28cee74250d1c7b4309f4568d0a8dd
NKR2609 <64198309+NKR2609@users.noreply.github.com>
2023-02-18 23:47:40 +0530

    Update Email.java

--- src/main/java/com/driver/Email.java
+++ src/main/java/com/driver/Email.java
@@ -1,5 +1,7 @@
 package com.driver;
 
+
+
 public class Email {
 
     private String emailId;
@@ -19,20 +21,37 @@ public class Email {
     }
 
     public void changePassword(String oldPassword, String newPassword){
-        if(password.equals(oldPassword)){
-            if(passWordValidation(newPassword)){
-                System.out.println("Password changed successfully");
+        //Change password only if the oldPassword is equal to current password and the new password meets all of the following:
+        // 1. It contains at least 8 characters
+        // 2. It contains at least one uppercase letter
+        // 3. It contains at least one lowercase letter
+        // 4. It contains at least one digit
+        // 5. It contains at least one special character. Any character apart from alphabets and digits is a special character
+        boolean containsUpperCase=false;
+        boolean containsLowerCase=false;
+        boolean containsADigit=false;
+        boolean containsSpecialChar=false;
+        if(oldPassword.equals(this.password)){
+
+            if(newPassword.length()<8) return; //checks if it contains atleat 8 characters
+
+            for(char c:newPassword.toCharArray()){
+                if(c>='A' && c<='Z') containsUpperCase=true;
+
+                if(c>='a' && c<='z') containsLowerCase=true;
+
+                if(c>='0' && c<='9') containsADigit=true;
+
+                if(!(c>='0' && c<='9') && !(c>='a' && c<='z') && !(c>='A' && c<='Z') ) containsSpecialChar=true;
+            }
+
+            //all criterials satisfied then change password
+            if(containsUpperCase && containsLowerCase && containsADigit && containsSpecialChar){
+
                 this.password=newPassword;
             }
-            else{
-                System.out.println("The new password is not valid!!!");
-            }
+            else return;
+
         }
-        else{
-            System.out.println("Your Current Password is not in our database");
-        }
-       }
-    private boolean passWordValidation(String password){
-        return password.length()>=8 && password.matches(".[A-Z].") && password.matches(".\\d.") && password.matches(".[^a-zA-Z0-9].");
     }
 }
